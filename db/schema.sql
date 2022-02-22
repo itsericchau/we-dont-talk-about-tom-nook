3477ab55b8726a992ec533687bdfc24aeb00cb5d
cchau <chun.wing.chau@gmail.com>
2022-02-23 04:28:44 +1100

    added fish

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,2 @@
 CREATE DATABASE we_hate_tom_nook_db;
+CREATE TABLE;
--- main.rb
+++ main.rb
@@ -1,8 +1,20 @@
 require "sinatra"
 require "sinatra/reloader" if development?
 require "pry" if development?
+require "httparty"
 
 require_relative "db/db"
+
 get "/" do
-  erb :index
+  fishes_api = HTTParty.get("https://acnhapi.com/v1/fish/")
+  erb :index,
+      locals: {
+        fishes_api: fishes_api,
+      }
+end
+
+get "/sign_up" do
+end
+
+get "/log_in" do
 end

77be487514f421af1f8fd90839ed4ff753dd650a
cchau <chun.wing.chau@gmail.com>
2022-02-23 11:27:19 +1100

    added controller, split views files

--- main.rb
+++ main.rb
@@ -9,47 +9,16 @@ enable :sessions
 require_relative "db/db"
 require_relative "models/users"
 require_relative "helpers/session_helper"
+require_relative "controllers/users_controller"
 
 get "/" do
   erb :index
 end
 
-get "/sign_up" do
-  erb :sign_up
-end
-
-get "/log_in" do
-  erb :log_in
-end
-
-post "/sign_up" do
-  username = params["username"]
-  email = params["email"]
-  password = params["password"]
-  create_user(username, email, password)
-  redirect "/"
-end
-
-post "/log_in" do
-  username = params["username"]
-  # email = params["email"]
-  password = params["password"]
-  user = find_user_by_username(username)
-  if user && BCrypt::Password.new(user["password_digest"]) == params["password"]
-    session["user_id"] = user["id"]
-    redirect "/"
-  end
-end
-
-delete "/sessions" do
-  session["user_id"] = nil
-  redirect "/"
-end
-
 get "/fishes" do
   fish = params["fish"]
   fishes_api = HTTParty.get("https://acnhapi.com/v1/fish/#{fish}")
-  erb :fishes,
+  erb :'pages/fishes',
       locals: {
         fishes_api: fishes_api,
       }
@@ -58,7 +27,7 @@ end
 get "/villagers" do
   villager = params["villagers"]
   villagers_api = HTTParty.get("https://acnhapi.com/v1/villagers/#{villager}")
-  erb :villagers,
+  erb :'pages/villagers',
       locals: {
         villagers_api: villagers_api,
       }
@@ -67,7 +36,7 @@ end
 get "/bugs" do
   bug = params["bugs"]
   bugs_api = HTTParty.get("https://acnhapi.com/v1/bugs/#{bug}")
-  erb :bugs,
+  erb :'pages/bugs',
       locals: {
         bugs_api: bugs_api,
       }
